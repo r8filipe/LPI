188ce9b7091bed52ddc2dd83a21493386c9f83e6
Henrique Vieira <28139@ufp.edu.pt>
2016-04-09 04:54:54 +0100

    Complete Models.

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+    public $timestamp = true;
+    protected $table = "categories";
+    protected $primarykey = 'id';
+    protected $fillable = [
+        'description'
+    ];
 }
