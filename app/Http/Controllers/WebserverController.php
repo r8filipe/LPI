52d3098bb2dbfa8f1a90f567d4d021d03338c79e
Filipe Vinha <r8filipe@gmail.com>
2016-04-13 00:58:56 +0100

    webserver input and validator

--- app/Http/Controllers/WebserverController.php
+++ app/Http/Controllers/WebserverController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Input;
+use Validator;
 use App\Http\Requests;
 
 class WebserverController extends Controller
